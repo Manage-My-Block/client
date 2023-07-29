1f0108ee6d9e73d26963ad665fd2a743cbe6fa2d
socialtonics <mjkgarrow@gmail.com>
2023-07-29 15:46:18 +1000

    created contact delete request

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -37,13 +37,22 @@ export async function updateContact(data) {
 }
 
 export async function createContact(data) {
-	console.log('Creating new Contact')
-
-	console.log(data)
-
 	try {
 
 		const response = await api.post('/contacts', data)
+		return response.data
+
+	} catch (error) {
+		console.log(error.message)
+	}
+}
+
+export async function deleteContact(data) {
+	console.log('Deleting a Contact')
+
+	try {
+		const response = await api.delete(`/contacts/${data}`,)
+
 		return response.data
 
 	} catch (error) {
