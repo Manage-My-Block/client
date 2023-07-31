f5c7581748f2f220144c5320322925b0ba868ff1
socialtonics <mjkgarrow@gmail.com>
2023-07-31 22:40:48 +1000

    can create multiple budgets and edit them

--- src/pages/BudgetPage.jsx
+++ src/pages/BudgetPage.jsx
@@ -7,13 +7,18 @@ import { useEffect, useState } from 'react'
 import BudgetFormModal from '../components/Budget/BudgetFormModal'
 import TransactionList from '../components/Budget/TransactionList'
 import TransactionFormModal from '../components/Budget/TransactionFormModal'
+import EditBudgetModal from '../components/Budget/EditBudgetModal'
 
 export default function BudgetPage() {
     const auth = useAuthUser()
     const buildingId = auth().user.building._id
 
     // Get Budget info
-    const budgetQuery = useQuery(['budget', buildingId], () => getBudgetByBuildingId(buildingId));
+    const budgetQuery = useQuery(['budgets', buildingId], () => getBudgetByBuildingId(buildingId));
+
+    function numberWithCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     if (budgetQuery.isLoading) return <LoadingIcon />
     if (budgetQuery.isError) return <h1>Error: {error.message}</h1>
@@ -21,59 +26,62 @@ export default function BudgetPage() {
     return (
         <div className='m-4'>
             <div>
-                <div className='flex justify-between'>
-                    <div>
-                        <h1 className='text-3xl font-extrabold'>{budgetQuery?.data?.name ? budgetQuery.data.name : "Budget"}</h1>
-                        <h1 className='text-secondary font-extrabold'>Balance: {budgetQuery?.data?.balance ? `$${budgetQuery.data.balance}` : ""}</h1>
-                    </div>
-
-                    {!budgetQuery.data &&
-                        <>
-                            <button
-                                // Open modal
-                                onClick={() => window.create_budget_modal.showModal()}
-                                className='btn btn-primary normal-case'
-                            >
-                                Create New Budget
-                            </button>
-                            <ModalDaisy modalId={'create_budget_modal'}>
-                                <BudgetFormModal />
-                            </ModalDaisy>
-
-                        </>
-
-                    }
-
-                    {budgetQuery.data &&
-                        <>
-                            <button
-                                // Open modal
-                                onClick={() => window.create_transaction_modal.showModal()}
-                                className='btn btn-primary normal-case'
-                            >
-                                Create New Transaction
-                            </button>
-
-                            <ModalDaisy modalId={'create_transaction_modal'}>
-                                <TransactionFormModal budgetId={budgetQuery.data._id} />
-                            </ModalDaisy>
-                        </>
-                    }
+                <button
+                    // Open modal
+                    onClick={() => window.create_budget_modal.showModal()}
+                    className='btn btn-primary normal-case'
+                >
+                    Create New Budget
+                </button>
+                <ModalDaisy modalId={'create_budget_modal'}>
+                    <BudgetFormModal />
+                </ModalDaisy>
 
 
+                {/* List budgets */}
+                {budgetQuery.data.length >= 1 && budgetQuery.data.map(budget => {
+                    return (
+                        <div key={budget._id} className='pt-10'>
+                            <div className='flex justify-between'>
+                                <div>
+                                    <h1 className='text-3xl font-extrabold'>{budget.name}</h1>
+                                    <h1 className='text-secondary font-extrabold'>Balance: {`$${numberWithCommas(budget.balance / 100)}`}</h1>
+                                </div>
 
+                                <button
+                                    // Open modal
+                                    onClick={() => window[`create_transaction_modal_${budget._id}`].showModal()}
+                                    className='btn btn-primary normal-case'
+                                >
+                                    Create New Transaction
+                                </button>
 
+                                <ModalDaisy modalId={`create_transaction_modal_${budget._id}`}>
+                                    <TransactionFormModal budgetId={budget._id} />
+                                </ModalDaisy>
 
-                </div>
+                                <button
+                                    // Open modal
+                                    onClick={() => window[`edit_budget_modal_${budget._id}`].showModal()}
+                                    className='btn btn-primary normal-case'
+                                >
+                                    Edit Budget
+                                </button>
+
+                                <ModalDaisy modalId={`edit_budget_modal_${budget._id}`}>
+                                    <EditBudgetModal budgetId={budget._id} />
+                                </ModalDaisy>
+                            </div>
+                            <div>
+                                <div className='mt-8 border border-neutral rounded'>
+                                    <TransactionList transactions={budget.transactions} />
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+
             </div>
-
-            {budgetQuery.data &&
-                <div className='mt-8 border border-neutral rounded'>
-                    <TransactionList transactions={budgetQuery.data.transactions} />
-                </div>
-            }
-
-
         </div>
     )
 }
