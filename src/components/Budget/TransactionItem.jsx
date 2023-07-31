607b1108bcb6093882cfe1195752a5d88e095a3a
socialtonics <mjkgarrow@gmail.com>
2023-07-31 21:38:24 +1000

    added tooltip to item description

--- src/components/Budget/TransactionItem.jsx
+++ src/components/Budget/TransactionItem.jsx
@@ -1,16 +1,23 @@
 import { startCase } from "lodash";
-import { cleanDateString } from "../../utils/helperFunctions";
+import { cleanDateString, shortenText } from "../../utils/helperFunctions";
 import { format } from "date-fns";
 
 
 export default function TransactionItem({ transaction }) {
+    function numberWithCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     return (
         <tr>
             <td className=''>{format(new Date(transaction.date), 'dd-MM-yyyy')}</td>
-            <td>{startCase(transaction.description)}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.tally}</td>
+            <td>
+                <div className="tooltip" data-tip={startCase(transaction.description)}>
+                    {shortenText(startCase(transaction.description), 10)}
+                </div>
+            </td>
+            <td>$ {numberWithCommas(transaction.amount / 100)}</td>
+            <td>$ {numberWithCommas(transaction.tally / 100)}</td>
         </tr>
     )
 }
